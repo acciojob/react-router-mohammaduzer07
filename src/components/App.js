2b528a083a91b352d927db646031a7c27435f959
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-07-04 16:50:51 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Home from "./Home";
+import About from "./About";
+import { Link } from "react-router-dom";
 
-const App = () => {
+const App = ({children}) => {
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul> 
+        {children}
+
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// import { createBrowserRouter, RouterProvider} from "react-router-dom"
+// import Home from "./components/Home";
+// import About from "./components/About";
+
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Home />
+//     },
+//     {
+//         path: "/about",
+//         element: <About />
+//     }
+// ])
+
+// // ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <RouterProvider router={router} />
+//     </React.StrictMode>
+// )
+
+
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./components/App";
+import Home from "./components/Home";
+import About from "./components/About";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </App>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
